214de9b81baa2c5f9c7bbe4bb91de22fcd0987a6
iminparallel <ch.harit1995@gmail.com>
2025-02-16 19:36:56 +0530

    updates types

--- contracts/MileStones.sol
+++ contracts/MileStones.sol
@@ -10,7 +10,7 @@ contract MileStones {
         uint256 amountWithdrawn;
         uint256 createdAt;
         bool isCompleted;
-        uint256 endedAt;
+        uint256 endsAt;
     }
 
     mapping(address => mapping( string => Milestone)) private milestones;
@@ -37,6 +37,11 @@ contract MileStones {
         _;
     }
 
+    modifier onlyCreator(string memory productId) {
+        require(milestones[msg.sender][productId].creator == msg.sender, "Only the creator can perform this action");
+        _;
+    }
+
     function lockFunds(string memory productId) external payable {
         require(msg.value > 0, "Funds must be greater than 0");
         require(milestones[msg.sender][productId].totalAmount == 0, "User already locked funds");
@@ -44,6 +49,7 @@ contract MileStones {
         uint256 fee = (msg.value * PLATFORM_PERCENTAGE) / 100;
         uint256 netAmount = msg.value - fee;
         owner_balance += fee;
+        uint256 endsAt = block.timestamp + 3600*24*7;
 
         //payable(platformWallet).transfer(fee);
 
@@ -54,13 +60,13 @@ contract MileStones {
             amountWithdrawn: 0,
             createdAt: block.timestamp,
             isCompleted: false,
-            endedAt: 0x000
+            endsAt: endsAt
         });
 
         emit FundsLocked(msg.sender, netAmount);
     }
 
-    function completeMilestone(string memory productId) external {
+    function completeMilestone(string memory productId) external onlyCreator(productId){
         Milestone storage milestone = milestones[msg.sender][productId];
         require(milestone.totalAmount > 0, "No funds locked");
         require(milestone.milestoneCompleted < MILESTONE_COUNT, "All milestones already completed");
@@ -85,7 +91,6 @@ contract MileStones {
             uint256 remainingAmount = milestone.totalAmount - milestone.amountWithdrawn;
             milestone.amountWithdrawn += remainingAmount;
             milestone.isCompleted = true;
-            milestone.endedAt = block.timestamp;
             payable(msg.sender).transfer(remainingAmount);
             emit AllFundsWithdrawn(msg.sender, milestone.totalAmount);
         } else {
